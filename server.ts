81ff3b6ef9b578ed034de5b3600c79a1b9c754fe
Alex Gleason <alex@alexgleason.me>
2023-03-26 18:58:17 -0500

    Refactor into separate functions

--- server.ts
+++ server.ts
@@ -1,14 +1,6 @@
 import { serve } from 'https://deno.land/std@0.181.0/http/server.ts';
 
-serve((req) => {
-  const upgrade = req.headers.get('upgrade') || '';
-
-  if (upgrade.toLowerCase() != 'websocket') {
-    return new Response('Please use a Nostr client to connect.');
-  }
-
-  const { socket, response } = Deno.upgradeWebSocket(req);
-
+function connectStream(socket: WebSocket): void {
   socket.onopen = () => console.log('socket opened');
 
   socket.onmessage = (e) => {
@@ -18,8 +10,19 @@ serve((req) => {
 
   socket.onerror = (e) => console.log('socket errored:', e);
   socket.onclose = () => console.log('socket closed');
+}
 
+function handleRequest(req: Request): Response {
+  const upgrade = req.headers.get('upgrade') || '';
+
+  if (upgrade.toLowerCase() != 'websocket') {
+    return new Response('Please use a Nostr client to connect.');
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  connectStream(socket);
   return response;
-}, {
-  port: 5000,
-});
+}
+
+serve(handleRequest, { port: 5000 });
