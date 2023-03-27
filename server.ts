8ac5a14a3d9bfb1740863476ecde3cbdd4f7afd7
Alex Gleason <alex@alexgleason.me>
2023-03-26 19:53:28 -0500

    Scaffoled out websocket handler

--- server.ts
+++ server.ts
@@ -1,15 +1,26 @@
 import { serve } from 'https://deno.land/std@0.181.0/http/server.ts';
+import { Event, Filter } from 'npm:nostr-tools@^1.7.4';
+
+type RelayMsg = ['EVENT', Event] | ['REQ', string, Filter] | ['CLOSE', string];
 
 function connectStream(socket: WebSocket): void {
-  socket.onopen = () => console.log('socket opened');
+  const subs = new Map<string, Filter>();
 
   socket.onmessage = (e) => {
-    console.log('socket message:', e.data);
-    socket.send(new Date().toString());
-  };
+    const msg: RelayMsg = JSON.parse(e.data);
+    console.log('Got message', msg);
 
-  socket.onerror = (e) => console.log('socket errored:', e);
-  socket.onclose = () => console.log('socket closed');
+    switch (msg[0]) {
+      case 'EVENT':
+        return;
+      case 'REQ':
+        socket.send(JSON.stringify(['EOSE', msg[1]]));
+        return;
+      case 'CLOSE':
+        subs.delete(msg[1]);
+        return;
+    }
+  };
 }
 
 function handleRequest(req: Request): Response {
